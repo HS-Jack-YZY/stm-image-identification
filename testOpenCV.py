acb681c63ac60f8110905c9eb04c00c026e9545f
HS-Jack-YZY <906581067@qq.com>
2024-01-09 19:39:27 +0800

    实现了OpenCV模板匹配功能
    在这个commit中，我们添加了使用OpenCV进行模板匹配的代码。
    我们首先加载了大图和小图，然后使用matchTemplate函数找到小图在大图中的位置。
    最后，我们使用矩形函数在大图上标记出小图的位置。

---
+++ testOpenCV.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import sys
+import os
+
+# 大图小图所在位置
+big_image_address = 'samples/big_image'
+small_image_address = 'samples/small_image'
+
+# 加载大图和小图
+img = cv2.imread(os.path.join(big_image_address, 'big_image.jpg'), 0)
+template = cv2.imread(os.path.join(small_image_address, 'small_image.jpg'), 0)
+
+# 获取小图的宽度和高度
+w, h = template.shape[::-1]
+
+# 使用matchTemplate函数来找到小图在大图中的位置
+res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+
+# 使用minMaxLoc函数来获取匹配结果的最大和最小值，以及它们的位置
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+# 使用矩形函数在大图上标记出小图的位置
+top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h)
+cv2.rectangle(img, top_left, bottom_right, 255, 2)
+
+# 显示结果
+cv2.imshow('Detected', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
